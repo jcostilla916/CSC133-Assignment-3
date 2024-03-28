8413904fa034b082ec11396e0b67c99e97171ce0
Vashisth <pprachi@csus.edu>
2024-03-27 22:52:47 -0700

    fixing code smell of snakeActivity, apple and Snake was fixed previously

--- app/src/main/java/com/example/c17snake/SnakeActivity.java
+++ app/src/main/java/com/example/c17snake/SnakeActivity.java
@@ -1,52 +1,42 @@
 package com.example.c17snake;
 
-import android.content.Context;
 import android.app.Activity;
 import android.graphics.Point;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Display;
-import android.widget.Button;
-import android.widget.TextView;
-
-import androidx.core.content.res.ResourcesCompat;
-
-import org.w3c.dom.Text;
-
-
 
 public class SnakeActivity extends Activity {
 
     // Declare an instance of SnakeGame
-    SnakeGame mSnakeGame;
+    private SnakeGame mSnakeGame;
 
-    // Set the game up
+    // Set up the game
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Get the pixel dimensions of the screen
+        // Get the display object to obtain screen dimensions
         Display display = getWindowManager().getDefaultDisplay();
 
-        // Initialize the result into a Point object
+        // Initialize the result into a Point object to store screen size
         Point size = new Point();
         display.getSize(size);
 
-        // Create a new instance of the SnakeEngine class
+        // Create a new instance of the SnakeGame class passing the context and screen size
         mSnakeGame = new SnakeGame(this, size);
 
-        // Make snakeEngine the view of the Activity
+        // Set SnakeGame as the content view of the activity
         setContentView(mSnakeGame);
     }
 
-    // Start the thread in snakeEngine
+    // Resume the game when the activity is resumed
     @Override
     protected void onResume() {
         super.onResume();
         mSnakeGame.resume();
     }
 
-    // Stop the thread in snakeEngine
+    // Pause the game when the activity is paused
     @Override
     protected void onPause() {
         super.onPause();
